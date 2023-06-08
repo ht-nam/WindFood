4a4c4ab5dc6cb4d4c11723e3347602b70623d4fc
Nam Hoang Thanh <hoangthanhnam10@gmail.com>
2023-06-08 07:33:10 +0700

    Add

--- Windfood_api/src/routers/provider.router.ts
+++ Windfood_api/src/routers/provider.router.ts
@@ -29,38 +29,6 @@ itemsRouter.get("/", async (req: Request, res: Response) => {
   }
 });
 
-// GET items/:id
-
-itemsRouter.get("/:id", async (req: Request, res: Response) => {
-  const id: number = parseInt(req.params.id, 10);
-
-  try {
-    const item: Provider | null = await ProviderService.findById(id);
-
-    if (item) {
-      return res.status(200).send(item);
-    }
-
-    res.status(404).send("item not found");
-  } catch (e) {
-    res.status(500).send((e as Error).message);
-  }
-});
-
-// POST items
-
-itemsRouter.post("/", async (req: Request, res: Response) => {
-  try {
-    const item: Provider = req.body;
-
-    const newItem = await ProviderService.saveOrUpdate(item);
-
-    res.status(201).json(newItem);
-  } catch (e) {
-    res.status(500).send((e as Error).message);
-  }
-});
-
 // PUT items/:id
 
 itemsRouter.put("/:id", async (req: Request, res: Response) => {
@@ -91,6 +59,24 @@ itemsRouter.delete("/:id", async (req: Request, res: Response) => {
     await ProviderService.remove(id);
 
     res.sendStatus(204);
+  } catch (e) {
+    res.status(500).send((e as Error).message);
+  }
+});
+
+// GET items/:id
+
+itemsRouter.get("/:id", async (req: Request, res: Response) => {
+  const id: number = parseInt(req.params.id, 10);
+
+  try {
+    const item: Provider | null = await ProviderService.findById(id);
+
+    if (item) {
+      return res.status(200).send(item);
+    }
+
+    res.status(404).send("item not found");
   } catch (e) {
     res.status(500).send((e as Error).message);
   }
