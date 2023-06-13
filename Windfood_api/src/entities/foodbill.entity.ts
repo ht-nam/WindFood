7ba10f7309d240461730a6b1f0b563d3e9e7823a
Nam Hoang Thanh <hoangthanhnam10@gmail.com>
2023-06-14 00:19:59 +0700

    m

--- Windfood_api/src/entities/foodbill.entity.ts
+++ Windfood_api/src/entities/foodbill.entity.ts
@@ -1,10 +1,10 @@
-import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn } from "typeorm";
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn, PrimaryGeneratedColumn } from "typeorm";
 import { Bill } from "./bill.entity";
 import { Food } from "./food.entity";
 
 @Entity({ name: "food_bill" })
 export class FoodBill {
-  @PrimaryColumn({ name: "food_bill_id" })
+  @PrimaryGeneratedColumn({ name: "food_bill_id" })
   foodBillId: number;
 
   @Column({ name: "quantity" })
