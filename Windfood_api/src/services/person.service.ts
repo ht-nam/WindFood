0a7fb14e9a19a06f827171b603937624343ae703
Nam Hoang Thanh <hoangthanhnam10@gmail.com>
2023-10-10 17:43:15 +0700

    Fix edit person info bugs

--- Windfood_api/src/services/person.service.ts
+++ Windfood_api/src/services/person.service.ts
@@ -38,10 +38,12 @@ export const getCurrentUser = async (token: string): Promise<object | null> => {
 
 export const saveOrUpdate = async (newItem: Person): Promise<Person> => {
   // newItem.hashedPassword = btoa(newItem.hashedPassword as string);
+  if (!newItem.personId && !newItem.password && newItem.password === "") {
+    newItem.password = (await findById(newItem.personId))?.password;
+  }
   newItem.password = await hashedPassword(
     newItem.password as string
   );
-
   // console.log(atob(newItem.hashedPassword as string));
   return personRepository.save(newItem);
 };
