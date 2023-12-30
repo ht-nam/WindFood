97468c6a819f5a41a1104e9feb99a6b43b9aa5bd
Vinh Van <tavanvinh172@gmail.com>
2023-12-30 22:41:59 +0700

    add payment dialog

--- Windfood_Client/app/src/main/java/com/project/windfood_client/ui/cart/PaymentActivity.java
+++ Windfood_Client/app/src/main/java/com/project/windfood_client/ui/cart/PaymentActivity.java
@@ -14,16 +14,23 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.project.windfood_client.R;
 import com.project.windfood_client.adapters.CartAdapter;
 import com.project.windfood_client.adapters.PaymentProductCardAdapter;
 import com.project.windfood_client.databinding.ActivityPaymentBinding;
 import com.project.windfood_client.models.Cart;
+import com.project.windfood_client.utils.Utils;
 
+import java.util.concurrent.Callable;
+
+import frenchtoast.FrenchToast;
+import frenchtoast.Mixture;
 import info.hoang8f.widget.FButton;
 
 public class PaymentActivity extends AppCompatActivity {
@@ -48,41 +55,9 @@ public class PaymentActivity extends AppCompatActivity {
         binding.payBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(PaymentActivity.this, R.style.DialogTheme);
-                builder.setTitle("Chọn phương thức thanh toán");
-                LinearLayout layout = new LinearLayout(PaymentActivity.this);
-                layout.setOrientation(LinearLayout.HORIZONTAL);
-                getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-                layoutParams.setMargins(0, 0, 50, 0); // Set left, top, right, bottom margins
-                FButton moneyPayment = new FButton(PaymentActivity.this);
-                moneyPayment.setTextColor(ContextCompat.getColor(PaymentActivity.this,R.color.white));
-                moneyPayment.setBackground(new ColorDrawable(Color.parseColor("#2980b9")));
-                moneyPayment.setText("Tiền mặt");
-                moneyPayment.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-//                        Toast.makeText(MainActivity.this, "You clicked OK.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-                FButton cashMoney = new FButton(PaymentActivity.this);
-                cashMoney.setText("Chuyển khoản");
-                cashMoney.setTextColor(ContextCompat.getColor(PaymentActivity.this,R.color.white));
-                cashMoney.setBackground(new ColorDrawable(Color.parseColor("#2980b9")));
-                cashMoney.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-//                        Toast.makeText(MainActivity.this, "You clicked Cancel.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                layout.addView(moneyPayment);
-                layout.addView(cashMoney);
-                builder.setView(layout);
-                builder.create().show();
+                Utils.showDialog(PaymentActivity.this);
             }
         });
-
     }
 
     public void initData(){
